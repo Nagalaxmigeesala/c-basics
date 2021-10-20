2edfb20f28cadb6fd006534a73b4af460f066041
Nagalaxmigeesala <92350257+Nagalaxmigeesala@users.noreply.github.com>
2021-10-20 12:38:51 +0530

    Add files via upload

---
+++ student details program.c
@@ -0,0 +1,19 @@
+/*c program to prints student details*/
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial; 
+	
+	//initilization
+	age=20;
+	cgpa=7.5;
+	initial='p';
+	
+	//out put
+	printf("student age is:%i\n", age);
+	printf("cgpa of the student is:%f\n",cgpa);
+	printf("student initial starts with:%c\n",initial);
+}
